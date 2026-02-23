f8cb809888e21b651c5a89ed8c4a4f36507484ec
Mario Aquino <marioaquino@MacBook-Pro.local>
2026-02-22 22:52:53 -0600

    fix: seo trailing slash redirection in vercel and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  site: 'https://todoesdigital.com',
+  trailingSlash: 'never', // Esto le dice a Astro que prefieres URLs sin la barra final
+});
 // https://astro.build/config
 export default defineConfig({});
+
